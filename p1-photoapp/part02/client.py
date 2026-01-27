abe2ba8a444dcaa5ca978bbc917daf5ab9b23c01
jacobwshg <wsg438@gmail.com>
2026-01-27 13:54:26 -0600

    p1 pt2 initial upload

---
+++ p1-photoapp/part02/client.py
@@ -0,0 +1,45 @@
+#
+# Simple client-side testing code for photoapp API functions.
+#
+# Initial code:
+#   Prof. Joe Hummel
+#   Northwestern University
+#
+
+import photoapp
+import sys
+
+import logging
+
+LOGFILE = 'log.txt'
+logging.basicConfig(
+	filename=LOGFILE,
+	level=logging.INFO,
+	format='%(asctime)s - %(levelname)s - %(message)s',
+	filemode='w'
+)
+
+#
+# eliminate traceback so we just get error message:
+#
+sys.tracebacklimit = 0
+
+print()
+print("**starting**")
+print()
+
+print("**initialize**")
+success = photoapp.initialize('photoapp-config.ini', 's3readwrite', 'photoapp-read-write')
+print(success)
+
+print()
+
+print("**get_ping**")
+(M,N) = photoapp.get_ping()
+print()
+print(f"M: {M}")
+print(f"N: {N}")
+
+print()
+print("**done**")
+print()
